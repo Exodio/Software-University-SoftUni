19c6bf35eeefd7312d8d863a9cf7c32e1ba7e42c
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 15:47:59 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/04. PB-CSharp-Nested-Conditional-Statements-Exercise/Visual Studio Code project/ex9onTimeForExam/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ex9onTimeForExamSecondOption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHour = int.Parse(Console.ReadLine());
+            int examMinute = int.Parse(Console.ReadLine());
+            int arrivalHour = int.Parse(Console.ReadLine());
+            int arrivalMinute = int.Parse(Console.ReadLine());
+
+            int totalExamTime = examHour * (60 + examMinute);
+            int totalArrivalTime = arrivalHour * (60 + arrivalMinute);
+            int diffEarly = totalExamTime - totalArrivalTime;
+            int diffLate = totalArrivalTime - totalExamTime;
+
+            int totalEarlyHour = diffEarly / 60;
+            int totalEarlyMinute = diffEarly % 60;
+
+            int totalLateHour = diffLate / 60;
+            int totalLateMinute = diffLate % 60;
+
+            if (totalExamTime < totalArrivalTime)
+            {
+                Console.WriteLine("Late");
+
+                if (diffLate < 60)
+                {
+                    Console.WriteLine($"{totalLateMinute} minutes after the start");
+                }
+                else if (diffLate >= 60)
+                {
+                    Console.WriteLine($"{totalLateHour}:{totalLateMinute:D2} hours after the start");
+                }
+            }
+            else if (diffEarly <= 30)
+            {
+                Console.WriteLine("On Time");
+                Console.WriteLine($"{totalEarlyMinute} minutes before the start");
+            }
+            else if (diffEarly > 30)
+            {
+                Console.WriteLine("Early");
+
+                if (diffEarly < 60)
+                {
+                    Console.WriteLine($"{totalEarlyMinute} minutes before the start");
+                }
+                else if (diffEarly >= 60)
+                {
+                    Console.WriteLine($"{totalEarlyHour}:{totalEarlyMinute:F2} hours before the start");
+                }
+            }
+          
+        }
+    }
+}
---
+++ Programming Basics with CSharp/04. PB-CSharp-Nested-Conditional-Statements-Exercise/Visual Studio Code project/ex9onTimeForExamSecondOptionDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace OnTimeForTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHour = int.Parse(Console.ReadLine());
+            int examMinute = int.Parse(Console.ReadLine());
+            int arrivalHour = int.Parse(Console.ReadLine());
+            int arrivalMinute = int.Parse(Console.ReadLine());
+
+            int totalExamTime = examHour * 60 + examMinute;
+            int totalArrivalTime = arrivalHour * 60 + arrivalMinute;
+            int diffEarly = totalExamTime - totalArrivalTime;
+            int diffLater = totalArrivalTime - totalExamTime;
+
+            int totalEarlyHour = diffEarly / 60;
+            int totalEarlyMin = diffEarly % 60;
+
+            int totalLaterHour = diffLater / 60;
+            int totalLaterMin = diffLater % 60;
+
+            if (totalExamTime < totalArrivalTime)
+            {
+                Console.WriteLine("Late");
+
+                if (diffLater < 60)
+                {
+                    Console.WriteLine($"{totalLaterMin} minutes after the start");
+                }
+                else if (diffLater >= 60)
+                {
+                    Console.WriteLine($"{totalLaterHour}:{totalLaterMin:D2} hours after the start");
+                }
+            }
+            else if (diffEarly <= 30)
+            {
+                Console.WriteLine("On Time");
+                Console.WriteLine($"{totalEarlyMin} minutes before the start");
+            }
+            else if (diffEarly > 30)
+            {
+                Console.WriteLine("Early");
+
+                if (diffEarly < 60)
+                {
+                    Console.WriteLine($"{totalEarlyMin} minutes before the start");
+                }
+                else if (diffEarly >= 60)
+                {
+                    Console.WriteLine($"{totalEarlyHour}:{totalEarlyMin:D2} hours before the start");
+                }
+            }
+        }
+    }
+}
