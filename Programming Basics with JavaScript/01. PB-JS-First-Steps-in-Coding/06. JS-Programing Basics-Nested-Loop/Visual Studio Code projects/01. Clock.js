c0556ecd9851d031ce1c1cac71c5f6ef9d52576d
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 19:01:47 +0300

    Delete 01. Clock.js

--- Programming Basics with JavaScript/01. PB-JS-First-Steps-in-Coding/06. JS-Programing Basics-Nested-Loop/Visual Studio Code projects/01. Clock.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-    for (let i = 0; i <= 23; i++) {
-        for (let j = 0; j <= 59; j++) {
-            console.log(`${i}:${j}`);
-        }
-        
-    }
-}
-solve([]);
