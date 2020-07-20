dcdf20ed657c51fc1aca90008e68a233f8863a3c
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 18:52:57 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/07. PB-CSharp-Nested-Loops-Lab/Visual Studio projects/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int a = 1; a < 3; a++)
+                {
+                    Console.WriteLine($"i = {i} & a = {a}");
+                }
+            }
+
+        }
+    }
+}
---
+++ Programming Basics with CSharp/07. PB-CSharp-Nested-Loops-Lab/Visual Studio projects/ex2multiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex2multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int one = 1; one <= 10; one++)
+            {
+                for (int two = 1; two <= 10; two++)
+                {
+                    int result = one * two;
+                    Console.WriteLine($"{one} * {two} = {result}");
+                }
+            }
+        }
+    }
+}
