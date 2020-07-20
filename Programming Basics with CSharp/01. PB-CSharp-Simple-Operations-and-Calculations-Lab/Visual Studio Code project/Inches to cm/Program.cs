75eddb86a16a2436da119241bc6d64ac1b543ea7
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 14:57:08 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/01. PB-CSharp-Simple-Operations-and-Calculations-Lab/Visual Studio Code project/Inches to cm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SquareArea
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Inches to cm calculation  ");
+            double cm = double.Parse(Console.ReadLine());
+            double inches = cm * 2.54;
+            Console.WriteLine("You have:" + inches + "cantemeters");
+
+            // OR IT CAN BE LIKE THIS
+
+            double inch = double.Parse(Console.ReadLine());
+            double centimeters = inch * 2.54;
+            Console.WriteLine(centimeters);
+
+        }
+    }
+}
---
+++ Programming Basics with CSharp/01. PB-CSharp-Simple-Operations-and-Calculations-Lab/Visual Studio Code project/PetShop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double DogsNumber = int.Parse(Console.ReadLine());
+            int OtherAnimal = int.Parse(Console.ReadLine());
+            double PriceDogs = DogsNumber * 2.50;
+            double PriceOtherAnimal = OtherAnimal * 4;
+            double AllPrice = PriceDogs + PriceOtherAnimal;
+
+            Console.WriteLine($"{AllPrice:F2} lv.");
+            
+
+
+
+        }
+    }
+}
