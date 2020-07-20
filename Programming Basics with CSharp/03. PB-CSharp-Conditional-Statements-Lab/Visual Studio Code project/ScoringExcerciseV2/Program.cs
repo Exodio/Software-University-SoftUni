739c5b1d87cdb67ec533ab4298f6561a8e8e8b16
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 15:20:12 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/03. PB-CSharp-Conditional-Statements-Lab/Visual Studio Code project/ScoringExcerciseV2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ScoringExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else if (true) //Proverqvane po konkretno uslovie (po striktna proverka)
+            { 
+  
+            }  
+            else
+            {
+                Console.WriteLine("Not excellent!");
+            }
+
+
+        }
+    }
+}
---
+++ Programming Basics with CSharp/03. PB-CSharp-Conditional-Statements-Lab/Visual Studio Code project/ScoringExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ScoringExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not excellent!");
+            }
+           
+         
+        }
+    }
+}
