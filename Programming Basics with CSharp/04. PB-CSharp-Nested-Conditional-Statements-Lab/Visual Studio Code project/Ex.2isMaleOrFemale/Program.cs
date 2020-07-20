32a1977186a492f445b1a5c6d9c8ad9d5d3c7bce
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 15:33:58 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/04. PB-CSharp-Nested-Conditional-Statements-Lab/Visual Studio Code project/Ex.2isMaleOrFemale/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex._2isMaleOrFemale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "f")
+            {
+                if (age < 16) 
+                {
+                    Console.WriteLine("Miss");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+            else
+            {
+                if (age <16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Programming Basics with CSharp/04. PB-CSharp-Nested-Conditional-Statements-Lab/Visual Studio Code project/ex.3LocalShotDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace ex._3LocalShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string productName = Console.ReadLine();
+            string town = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (town == "Sofia")
+            {
+                if (productName == "coffe")
+                {
+                    price = quantity * 0.50;
+                }
+                else if (productName == "water")
+                {
+                    price = quantity * 0.80;
+
+                }
+                else if (productName == "beer")
+                {
+                    price = quantity * 1.20;
+
+                }
+                else if (productName == "sweets")
+                {
+                    price = quantity * 1.45;
+
+                }
+                else if (productName == "peanuts")
+                {
+                    price = quantity * 1.60;
+                }
+                Console.WriteLine(price);
+
+            }
+            else if (town == "Plovdiv")
+            {
+                if (productName == "coffe")
+                {
+                    price = quantity * 0.40;
+                }
+                else if (productName == "water")
+                {
+                    price = quantity * 0.70;
+
+                }
+                else if (productName == "beer")
+                {
+                    price = quantity * 1.15;
+
+                }
+                else if (productName == "sweets")
+                {
+                    price = quantity * 1.45;
+
+                }
+                else if (productName == "peanuts")
+                {
+                    price = quantity * 1.50;
+                }
+                Console.WriteLine(price);
+            }
+            else if (town == "Varna")
+            {
+                if (productName == "coffe")
+                {
+                    price = quantity * 0.45;
+                }
+                else if (productName == "water")
+                {
+                    price = quantity * 0.70;
+
+                }
+                else if (productName == "beer")
+                {
+                    price = quantity * 1.10;
+
+                }
+                else if (productName == "sweets")
+                {
+                    price = quantity * 1.35;
+
+                }
+                else if (productName == "peanuts")
+                {
+                    price = quantity * 1.55;
+                }
+                Console.WriteLine(price);
+            }
+        }
+    }
+}
