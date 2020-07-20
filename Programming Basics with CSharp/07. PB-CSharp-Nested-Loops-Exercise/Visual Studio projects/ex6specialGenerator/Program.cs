ee6c1cbdccda887c9e374528ad7a9cb9089b3170
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 18:46:52 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/07. PB-CSharp-Nested-Loops-Exercise/Visual Studio projects/ex6specialGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex6specialGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int a = 1; a <= 9; a++)
+                {
+                    for (int b = 1; b <= 9; b++)
+                    {
+                        for (int c = 1; c <= 9; c++)
+                        {
+                            if (n % i == 0 && n % a == 0 && n % b == 0 && n % c == 0)
+                            {
+                                Console.Write("{0}{1}{2}{3} ", i, a, b, c);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming Basics with CSharp/07. PB-CSharp-Nested-Loops-Exercise/Visual Studio projects/nestedLoopsExcersise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nestedLoopsExcersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
