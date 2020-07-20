a971011c573a2d298759978bcd1be286c6d838a6
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 15:26:07 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/03. PB-CSharp-Conditional-Statements-Lab/Visual Studio Code project/SimpleConditions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "eva123";
+            string username = "Welcome back Eva!";
+            Console.WriteLine("Please enter your password: ");
+            string passowrdInput = Console.ReadLine();
+            
+
+            if (password == passowrdInput) 
+            {
+                Console.WriteLine((username!));
+            }
+            else 
+            {
+                Console.WriteLine("Incorrect Password");
+            }
+
+           
+
+
+        }
+    }
+}
