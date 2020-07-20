f0373e0fd416c0899faef8b07c99d737ba931fe8
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 14:50:06 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/01. PB-CSharp-Simple-Operations-and-Calculations-Lab/Visual Studio Code project/Architect Project Creation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Architect_Project_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int ProjectCount = int.Parse(Console.ReadLine());
+            int ProjectHours = ProjectCount * 3;
+
+            Console.WriteLine($"The architect {architectName} will need {ProjectHours} hours to complete {ProjectCount} project/s.");
+
+        }
+    }
+}
