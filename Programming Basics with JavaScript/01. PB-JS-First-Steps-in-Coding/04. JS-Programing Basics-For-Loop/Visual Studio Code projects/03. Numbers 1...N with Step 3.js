166284f434b91a8dd8494be8707218d1cb39fb48
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 18:59:22 +0300

    Delete 03. Numbers 1...N with Step 3.js

--- Programming Basics with JavaScript/01. PB-JS-First-Steps-in-Coding/04. JS-Programing Basics-For-Loop/Visual Studio Code projects/03. Numbers 1...N with Step 3.js
+++
@@ -1,9 +0,0 @@
-function number1ToNWith3Steps(input) {
-    let n = Number(input.shift());
-
-    for (let i = 1; i <= n; i += 3) {
-        console.log(i);
-
-    }
-}
-number1ToNWith3Steps(["10"]);
