348ee37aaef8ef277623452fcbe35964b4bf15ba
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 15:10:32 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/03 PB-CSharp-Conditional-Statements-Exercise/Visual Studio Code project/exSeverWorldSwimmingRecord/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exSeverWorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordTime = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeInSeconds = double.Parse(Console.ReadLine());
+
+            double totalSeconds = distanceInMeters * timeInSeconds;
+            double delaySeconds = Math.Floor(distanceInMeters / 15) * 12.5;
+
+            double totalSeocndsTime = totalSeconds + delaySeconds;
+
+            if (totalSeocndsTime < recordTime)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalSeocndsTime:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalSeocndsTime - recordTime:F2} seconds slower.");
+            }
+
+        }
+    }
+}
---
+++ Programming Basics with CSharp/03 PB-CSharp-Conditional-Statements-Exercise/Visual Studio Code project/exTimePlus15Minutes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TimePlus15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            //mintues +=15
+            minutes += 15;
+
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours += 1;
+            }
+
+            if (hours >= 24)
+            {
+                hours -= 24;    
+            }
+
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        
+        }
+    }
+}
