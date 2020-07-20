f51f11212b6c54626a27713d5243c0058c658831
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 20:40:22 +0300

    Add files via upload

---
+++ Programming Fundamentals with CSharp/01. CSF-IntroAndBasicSyntrax/VS projects/ex5foreightLanguage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex5foreightLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string languageOrigin = Console.ReadLine();
+
+            if (languageOrigin == "England" || languageOrigin == "USA")
+            {
+                Console.WriteLine("English");
+            }
+            else if (languageOrigin == "Spain" || languageOrigin == "Argentina" || languageOrigin == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+
+        }
+    }
+}
---
+++ Programming Fundamentals with CSharp/01. CSF-IntroAndBasicSyntrax/VS projects/ex7divisibleByThree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex7divisibleByThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            for ( int i = 3; i <= 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
