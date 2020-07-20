66b885f0de99390053e425093d4e3521f1574528
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 19:02:10 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/08. Exam-Preparation/Visual Studio projects/ex5dartsTour/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ex5dartsTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingPoints = int.Parse(Console.ReadLine());
+            int movesCount = 0;
+
+            int currentSectorCount = startingPoints; //!Added to avoid confusion afterwards. We save the current points that are received and later on will be remove, depending on the target sector, that has been hit below.
+
+
+            while (currentSectorCount > 0)
+            {
+                movesCount++; //Adding +1 should be here instead of down, since we need +1 in the beginning as it will show one more additional after the program runs.
+
+                string targetSector = Console.ReadLine();   //Target sector.
+
+
+                if (targetSector == "bullseye")  //We place the if statemnt here, because if there is a bulleye by exercise design the program should terminate (there is more in next sentence).
+                {
+                    Console.WriteLine($"Congratulations! You won the game with a bullseye in {movesCount} moves!");  // !If we do not paste it here we will get an error in the upcoming: int currentPoints = int.Parse(Console.ReadLine()); due to the fact that it will have to read another number and in our case there won't be one as we already concluded termination.
+
+                    return;
+                }
+
+
+                int currentHitPoints = int.Parse(Console.ReadLine()); //! If there is no bullseye we can continue by checking the points hit.
+
+
+                if (targetSector == "number section")
+                {
+                    currentSectorCount = currentSectorCount - currentHitPoints;  //Target sector actions taken and saved in the above currentSectorCount.
+                }
+                else if (targetSector == "double ring")
+                {
+                    currentSectorCount = currentSectorCount - (currentHitPoints * 2);
+                }
+                else if (targetSector == "triple ring")
+                {
+                    currentSectorCount = currentSectorCount - (currentHitPoints * 3);
+                }
+            }
+
+            if (currentSectorCount == 0)
+            {
+                Console.WriteLine($"Congratulations! You won the game in {movesCount} moves!");
+            }
+            else if (currentSectorCount < 0)  //As we are going to have a negative number as a result here, we would need to use Math.Abs, which will turn a negative number into a postiive one, as required in this exercise.
+            {
+                Console.WriteLine($"Sorry, you lost. Score difference: {Math.Abs(currentSectorCount)}.");
+            }
+        }
+    }
+}
---
+++ Programming Basics with CSharp/08. Exam-Preparation/Visual Studio projects/ex6bakingCompetition/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ex6bakingCompetition
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int participantsCount = int.Parse(Console.ReadLine()); //1st role input.
+
+            int totalBakedGoodsCount = 0; //exit request 1 (input below in the for loop).
+            double totalSum = 0; //exit request 2 (input below in the for loop).
+
+            double cookiesPrice = 1.50;
+            double cakesPrice = 7.80;
+            double wafflesPrice = 2.30;
+
+            for (int i = 0; i < participantsCount; i++) //! In this case [i] is not [<(=)] [participantsCount] as it will have another run after the Stop baking input and it will result in endless loop.
+            {
+                int cookiesCount = 0;
+                int cakesCount = 0;
+                int wafflesCount = 0;
+
+                string participantName = Console.ReadLine(); //2nd role input.
+                string bakeryType = Console.ReadLine(); //3rd role input.
+
+                while (bakeryType != "Stop baking!")
+                {
+                    int bakeryCount = int.Parse(Console.ReadLine());  //4th role input.
+
+                    if (bakeryType == "cookies")
+                    {
+                        cookiesCount = cookiesCount + bakeryCount;
+                    }
+                    else if (bakeryType == "cakes")
+                    {
+                        cakesCount = cakesCount + bakeryCount;
+
+                    }
+                    else if (bakeryType == "waffles")
+                    {
+                        wafflesCount = wafflesCount + bakeryCount;
+
+                    }
+                    bakeryType = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{participantName} baked {cookiesCount} cookies, {cakesCount} cakes and {wafflesCount} waffles."); //exit input 1
+
+                totalBakedGoodsCount = totalBakedGoodsCount + (cookiesCount + cakesCount + wafflesCount); //total baked goods count exit request:1
+                totalSum = totalSum +((cookiesCount * cookiesPrice) + (cakesCount * cakesPrice) + (wafflesCount * wafflesPrice)); //total amount gathered for charity exit request:2
+            }
+
+            Console.WriteLine($"All bakery sold: {totalBakedGoodsCount}"); //exit input 2   
+            Console.WriteLine($"Total sum for charity: {totalSum:F2} lv."); //exit input 3
+        }
+    }
+}
