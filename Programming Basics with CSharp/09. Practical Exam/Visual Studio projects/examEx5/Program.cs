f62b7942cb2afb95b53125d54fe6f67c22dc3789
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 19:08:34 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/09. Practical Exam/Visual Studio projects/examEx5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace examEx5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+
+            int totalVolume = width * length * hight;
+            int volumeNeeded = 0;
+
+
+            string input = Console.ReadLine();
+
+            while (input != "Done")
+            {
+                volumeNeeded = (volumeNeeded + int.Parse(input));
+
+                if (volumeNeeded > totalVolume)
+                {
+                    break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (input == "Done")
+            {
+                Console.WriteLine($"{totalVolume - volumeNeeded} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {volumeNeeded - totalVolume} Cubic meters more.");
+            }
+        }
+    }
+}
---
+++ Programming Basics with CSharp/09. Practical Exam/Visual Studio projects/examEx6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace pbExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numOneUpperEnd = int.Parse(Console.ReadLine());
+            int numTwoUpperEnd = int.Parse(Console.ReadLine());
+            int numThreeUpperEnd = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numOneUpperEnd; i++)
+            {
+                for (int k = 1; k <= numTwoUpperEnd; k++)
+                {
+                    for (int j = 1; j <= numThreeUpperEnd; j++)
+                    {
+                        if (i % 2 == 0 && j % 2 == 0)
+                        {
+                            if (k == 2 || k == 3 || k == 5 || k == 7)
+                            {
+                                Console.WriteLine($"{i} {k} {j}");
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+}
