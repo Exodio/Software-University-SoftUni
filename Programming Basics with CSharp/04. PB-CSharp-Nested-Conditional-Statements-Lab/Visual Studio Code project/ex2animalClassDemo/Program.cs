51cfd3299dca8435586ee96f6107d15628b87ef8
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-07-20 15:37:09 +0300

    Add files via upload

---
+++ Programming Basics with CSharp/04. PB-CSharp-Nested-Conditional-Statements-Lab/Visual Studio Code project/ex2animalClassDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex2animalClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            switch (animal) //we use that as an if
+            {
+
+                case "dog": Console.WriteLine("mammal");break; //this is the if statement
+                case "crocodile": //those are the else if statements 
+                case "tortoise":
+                case "snake":
+                Console.WriteLine("reptile");break; //since they are all under the same entitlement, we have put only one break for all
+
+                default: //we use that as an else
+                Console.WriteLine("unknown");break;
+            }
+
+        }
+    }
+}
