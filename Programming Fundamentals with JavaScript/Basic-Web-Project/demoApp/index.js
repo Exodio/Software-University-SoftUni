2a8621b619ac8e1ac8004e5612f39a6a08a87c55
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-08-14 15:02:26 +0300

    Test exercise finish

--- Programming Fundamentals with JavaScript/Basic-Web-Project/demoApp/index.js
+++ Programming Fundamentals with JavaScript/Basic-Web-Project/demoApp/index.js
@@ -3,26 +3,37 @@
 //npm install express --save //used in terminal to intiate the server actions
 //np install express-nahdlebars --save //Our templated HTML server response
 const express = require("express"); //main request initiator
-const handlebars = require('express-handlebars'); //main engine set
+const handlebars = require("express-handlebars"); //main engine set
 
-app.engine('hbs', handlebars({ //handlebars configuration
-    layoutsDir: 'views',
-    defaultLayout: 'main-layout',
-    partialsDir: 'views/partials',
-    extname: 'hbs',
-}));
+app.engine(
+    "hbs",
+    handlebars({
+        //handlebars configuration
+        layoutsDir: "views",
+        defaultLayout: "main-layout",
+        partialsDir: "views/partials",
+        extname: "hbs",
+    })
+);
 app.set("view engine", "hbs"); //we define our handlebars expression
 
 const app = express(); //calling out the function above
 const port = 3013; //local free port
 
-app.render("/", function(req, res){ //we call the function to check in ours views folder for the index hbs
-    res.render("index", {layout: false});
-})
-app.get("/greeting", function (req, res) { //we define action - answers requests
+app.render("/", function (req, res) {
+    //we call the function to check in ours views folder for the index hbs
+    res.render("index", {
+        layout: false, //handlebars express turn off layout
+        name: "User", //Username specified in the hbs index
+        age: Math.floor(Math.random() * 100), //Random output of number(age) specified in the hbs index
+    }); //Render by view engine the hbs html (by the index hbs file)(handlebars express turn off layout)(specified name shown in the name tab)
+});
+app.get("/greeting", function (req, res) {
+    //we define action - answers requests
     res.send("Hey there user"); //once request is called the following answer will be received
 });
-app.get("/ask", (req, res) => { //action definiton with arrow function
+app.get("/ask", (req, res) => {
+    //action definiton with arrow function
     res.send("What is your name ?");
 });
 app.listen(port, () => console.log(`Listening on port ${port}...`)); //It checks if any actions are required, we declare it to always be checking and reviewing if actions are requred
