cd621c1e92bb9689dc7b57b5731033e77f0e21b0
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-10-17 20:20:45 +0300

    Lab exercise finish

--- Advanced Programming with JavaScript/09. JS-Advanced-Prototypes-and-Inheritance/VSC projects/03. Extend Prototype.js
+++ Advanced Programming with JavaScript/09. JS-Advanced-Prototypes-and-Inheritance/VSC projects/03. Extend Prototype.js
@@ -8,5 +8,19 @@ function extendProrotype(classToExtend) {
     }
 }
 
+//input
+class Species {
+    isSpecies = true;
+
+    toString() {
+        return "Human";
+    }
+}
+extendPrototype(Species);
+
+let species = new Species();
+console.log(species.toSpeciesString());
+
+//output
 //new classToExtend("Maria", "maria@gmail.com").toSpeciesString()
 // "I am a Human. Person (name: Maria, email: maria@gmail.com)"
