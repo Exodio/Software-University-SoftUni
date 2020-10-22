a7cce9b44f0e5d1c6a2250f65af97e713a7f4e51
Adrian Minchev <68440580+Exodio@users.noreply.github.com>
2020-10-22 20:09:03 +0300

    Exam preparation++

--- Advanced Programming with JavaScript/10. Exam-Preparation/JS Advanced Exam - 12.08.2020/02. Halls/solution.js
+++ Advanced Programming with JavaScript/10. Exam-Preparation/JS Advanced Exam - 12.08.2020/02. Halls/solution.js
@@ -1,4 +1,139 @@
-function solve() {
-    // TODO ...
+function solveClasses() {
+    class Hall {
+        constructor(capacity, name) {
+            this.capacity = capacity;
+            this.name = name;
+            this.events = [];
+
+        }
+
+        hallEvent(title) {
+
+            if (!this.events.includes(title)) { //check if even is added and if it included in the array
+                this.events.push(title); //if not add it
+                return "Event is added.";
+            }
+
+            throw new Error("This event is already added!"); //else throw error
+        }
+
+        close() {
+            this.events = []; //clears out the events array
+            return `${this.name} hall is closed.`;
+        }
+
+        toString() {
+            let output = []; // create a storage for the events
+            output.push(`${this.name} hall - ${this.capacity}`); //add the event template for each data to the storage
+
+            if (this.events.length > 0) { //if there are any events in the arr add them to the storage arr
+                output.push(`Events: ${this.events.join(", ")}`);
+            }
+
+            return output.join("\n"); //print each event on a new line
+        }
+    }
+
+    class MovieTheater extends Hall {
+        constructor(capacity, name, screenSize) {
+            super(capacity, name);
+            this.screenSize = screenSize;
+            this.events = [];
+        }
+
+        close() {
+            return super.close() + "All screenings are over."; //inherits the close() method and returns the required message on the same line
+        }
+
+        toString() {
+            super.toString(); // extend the previous function toString
+            let output = []; //create new storage location
+
+            output.push(super.toString()); //add the previous data to the new empty storage
+            output.push(`${this.name} is a movie theater with ${this.screenSize} screensize and ${this.capacity} seats capacity.`); //add the new string elements to the new storage location that has been extended from the previous one
+
+            return output.join("\n"); //print new output on a new line
+        }
+    }
+
+    class ConcertHall extends Hall {
+        constructor(capacity, name) {
+            super(capacity, name);
+            this.events = [];
+            this.performers = []; //!
+        }
+
+        hallEvent(title, performers) { //name is mistakenly made it is hallEvent not hallEvents as listed in the description
+
+            if (!this.events.includes(title)) { //checks if we have not yet inherited already the new title property
+                this.events.push(title); //adding new title to the events array
+                this.events.push(performers); //also creating new property to the events array [events[name][capacity][PERFORMERS]];
+                this.performers.push(performers); //!add the date of performers into the new array for future use
+                return "Event is added.";
+            }
+
+            throw new Error("This event is already added!");
+        }
+
+        close() {
+            return super.close() + "All performers are over."; //inherits the close() method and returns the required message on the same line
+        }
+
+        toString() {
+            super.toString(); // extend the previous function toString
+            let output = []; //create new storage location
+            output.push(super.toString()); //adds the data from the extension of the toString and its data to the new empty storage
+
+            if (this.events.length > 0) { // if there are any events to the event array
+                for (const eachEvent of this.performers) { //go through each indication of event in the saved performers array, as property performers in events is not itterible
+                    output.push(`Performers: ${eachEvent.join(", ")}.`); //add it to the new output storage and print each event saparated with space
+                }
+            }
+
+            return output.join("\n"); //join each line event on a new line
+        }
+    }
+
+    return {
+        Hall,
+        MovieTheater,
+        ConcertHall,
+    }
 }
 
+//input
+let classes = solveClasses();
+let hall = new classes.Hall(20, 'Main');
+console.log(hall.hallEvent('Breakfast Ideas'));
+console.log(hall.hallEvent('Annual Charity Ball'));
+console.log(hall.toString());
+console.log(hall.close());
+//--------------------------------------------------------------------------------------
+let movieHall = new classes.MovieTheater(10, 'Europe', '10m');
+console.log(movieHall.hallEvent('Top Gun: Maverick'));
+console.log(movieHall.toString());
+//--------------------------------------------------------------------------------------
+let concert = new classes.ConcertHall(5000, 'Diamond');
+console.log(concert.hallEvent('The Chromatica Ball', ['LADY GAGA']));
+console.log(concert.toString());
+console.log(concert.close());
+console.log(concert.toString());
+
+//output
+// Event is added.
+// Event is added.
+// Main hall - 20
+// Events: Breakfast Ideas, Annual Charity Ball
+// Main hall is closed.
+// --------------------------------------------------------------------------------------
+// Event is added.
+// Europe hall - 10
+// Events: Top Gun: Maverick
+// Europe is a movie theater with 10m screensize and 10 seats capacity.
+// --------------------------------------------------------------------------------------
+// Event is added.
+// Diamond hall - 5000
+// Events: The Chromatica Ball
+// Performers: LADY GAGA.
+// Diamond hall is closed.Аll performances are over.
+// Diamond hall - 5000
--- Advanced Programming with JavaScript/10. Exam-Preparation/JS Advanced Exam - 27.07.2020/01. Pet Me/solution.js
+++ Advanced Programming with JavaScript/10. Exam-Preparation/JS Advanced Exam - 27.07.2020/01. Pet Me/solution.js
@@ -1,4 +1,90 @@
 function solve() {
-    // TODO ...
-}
 
+    let buttonAdd = document.querySelector("#container button"); //take the button in the container section with name button, you can use getElementByTagName
+    let inputSection = document.querySelectorAll("#container input"); //get all the elements inputs of the container
+    let inputs = Array.from(inputSection); //Converting an HTML collection into an array IN JUDGE IT WILL BE NEEDED
+    let [nameInput, ageInput, kindInput, ownerInput] = inputs; //we take each element with spread opperator from the array of inputs in order to use them individually
+
+    let adoptionSection = document.querySelector("#adoption ul"); //take all of the adoption ul elements
+    let adoptedSection = document.querySelector("#adopted ul"); //take all of the adopted ul elements
+
+    buttonAdd.addEventListener("click", (e) => {
+        //first click function
+        //we want to take the the input data once the button is clicked
+        e.preventDefault();
+
+        if (!inputs.every((x) => x.value)) {
+            //we check if every element has a value different than ""
+            //console.log("Not full inputs added"); //check
+            return;
+        }
+
+        if (!Number(ageInput.value)) {
+            //check if the age input is a Number value, alternative isNaN(Number(ageInput.age));
+            return;
+        }
+
+        let li = document.createElement("li"); //we create a new li element
+        let p = document.createElement("p"); //we create new p element
+        let span = document.createElement("span"); //we create new span element
+        let contactButton = document.createElement("button"); //we create new button element
+
+        p.innerHTML = `<strong>${nameInput.value}</strong> is a <strong>${ageInput.value}</strong> year old <strong>${kindInput.value}</strong>`;
+        span.textContent = `Owner: ${ownerInput.value}`; // we add the owner input to the span element
+        contactButton.textContent = "Contact with owner"; //we add the text to the contact button
+
+        li.appendChild(p); //we append the p to the li
+        li.appendChild(span); //we append the span element once we have the valid input from above and we attach it to the adoption section
+        li.appendChild(contactButton); //we append the button element to the li of the adoption section
+        adoptionSection.appendChild(li); //we append the li to the next section from the input
+
+        nameInput.value = ""; // we clear each value of the inputs
+        ageInput.value = "";
+        kindInput.value = "";
+        ownerInput.value = "";
+
+        contactButton.addEventListener("click", (e) => {
+            //second click function
+            let liParentElement = e.currentTarget.parentElement; //we take the button which exactly we have clicked, which in our case is the li
+            e.currentTarget.remove(); //we remove the current button element
+
+            let div = document.createElement("div"); //we create the new elements
+            let input = document.createElement("input");
+            let takeItButton = document.createElement("button");
+
+            input.setAttribute("placeholder", "Enter your names"); //we set a new attribute with the name: placeholder, and the following text
+            takeItButton.textContent = "Yes! I take it!"; // once the button is clicked the text on it changes
+
+            div.appendChild(input); //append the input to the div
+            div.appendChild(takeItButton); //append the div into the take button
+            liParentElement.appendChild(div); //we took the parrent in the beginning in order to append the new div
+
+            takeItButton.addEventListener("click", (e) => {
+                let entireLiElement = e.currentTarget.parentElement.parentElement; //we take the button which exactly we have clicked, which in our case is the li and we take the inner new div li"s data
+                let parentButtonEl = e.currentTarget.parentElement; //we remove the previous saved button element
+                let newOwnerInputElement = entireLiElement.querySelector("input"); //takes the text for the owner's name
+                let ownerNameSpanElement = entireLiElement.querySelector("span"); //takes the text for the new owner's name
+
+                let newOwnerName = newOwnerInputElement.value; //changes the new span of the owner element with the new text
+
+                if (!newOwnerName) { //if there is no owner name input
+                    return;
+                }
+
+                ownerNameSpanElement.textContent = `New Owner: ${newOwnerName}`; //we set the new owner's name
+
+                adoptedSection.appendChild(entireLiElement); //we append the li element with all the previous data to the last adopted section, this will automatically move the ul data without the necessity to delete anything!
+                parentButtonEl.remove(); //we remove the previous saved button element
+
+                let checkedButtonElement = document.createElement("button"); //we create the checked button element
+                checkedButtonElement.textContent = "Checked"; //we set it's text
+
+                entireLiElement.appendChild(checkedButtonElement); //we append it to our main li element
+
+                checkedButtonElement.addEventListener("click", (e) => {
+                    e.currentTarget.parentElement.remove(); //we delete the current item listed (li)(selected) element or we can use parentButtonEl.remove();
+                });
+            });
+        });
+    });
+}
